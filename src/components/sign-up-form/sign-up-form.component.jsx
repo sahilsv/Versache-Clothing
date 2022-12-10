bcc67d8c93afc8c6fdacf23eefbd9746bb91c4fd
Sahil Singh Virdhi <929102@cognizant.com>
2022-12-10 19:29:44 +0530

    website with homepage navigation, authentication functionality and product checkout and modification functionality added

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,93 @@
+import { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
+
+import './sign-up-form.styles.scss';
+
+const defaultFormFields = {
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+const SignUpForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { name, email, password, confirmPassword } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if(password !== confirmPassword) {
+        alert('passwords do not match');
+        return;
+    }
+
+    try {
+        const {user} = await createAuthUserWithEmailAndPassword(email, password);
+
+        await createUserDocumentFromAuth(user, {name});
+        resetFormFields();
+    } catch(error) {
+        if(error.code === 'auth/email-already-in-use') {
+            alert('Cannot create user, email already in use');
+        } else{
+            console.log(error.message);
+        }
+    }
+  }
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+
+  return (
+    <div className="sign-up-container">
+      <h2>Don't have an account?</h2>
+      <span>Sign up with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        {/* <label>Name</label> */}
+        <FormInput label="Name" type="text" onChange={handleChange} name="name" value={name} />
+
+        {/* <label>E-mail</label> */}
+        <FormInput
+          label="E-mail"
+          type="email"
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        {/* <label>Password</label> */}
+        <FormInput
+          label="Password"
+          type="password"
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+
+        {/* <label>Confirm Password</label> */}
+        <FormInput
+          label="Confirm Password"
+          type="password"
+          onChange={handleChange}
+          name="confirmPassword"
+          value={confirmPassword}
+        />
+
+        <Button type="submit">Sign Up</Button>
+      </form>
+    </div>
+  );
+};
+
+export default SignUpForm;
---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,98 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBn5fuSbqneBhtNOwn6BPTUV3mM3Q0N5Vg",
+  authDomain: "versache-clothing-db.firebaseapp.com",
+  projectId: "versache-clothing-db",
+  storageBucket: "versache-clothing-db.appspot.com",
+  messagingSenderId: "492796702718",
+  appId: "1:492796702718:web:322f8f9fc7d666e53b5aaf",
+  measurementId: "G-8F9JX4FQ3Y",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Gives back a provider instance
+const googleProvider = new GoogleAuthProvider();
+
+// setCustomParameters takes in certain type of configuration objects and with it we can tell the
+// different ways we want this Google Auth provider to behave
+// we use 'prompt' bcoz everytime the user interacts with the provider, we want to force them to
+// select an account
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+// no 'new' keyword since it is to be instantiated only once
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (
+  userAuth,
+  additionalInformation = {}
+) => {
+  if (!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  // this userSnapshot tells us whether an instance of it exists in the database or not
+  // and it also allows us to access the data
+  const userSnapshot = await getDoc(userDocRef);
+  // console.log(userSnapshot);
+  // console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { name, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        name,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+
+  return await signInWithEmailAndPassword(auth, email, password);
+};
+
+export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
