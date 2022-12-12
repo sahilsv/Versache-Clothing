3e8b8d75a19aa3c1bbbf98770e2fb438bd4e1729
Sahil Singh Virdhi <sahilsv003@gmail.com>
2022-12-12 23:28:46 +0530

    assigned different routes for different categories in Shop page

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,26 @@
+import { useContext, Fragment } from "react";
+
+import CategoryPreview from "../../components/category-preview/category-preview.component";
+import { CategoriesContext } from "../../contexts/categories.context";
+
+import "./categories-preview.styles.scss";
+
+const CategoriesPreview = () => {
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  return (
+    <Fragment>
+      {
+        // gives back an array of all key values (hats, ..)
+        Object.keys(categoriesMap).map((title) => {
+          const products = categoriesMap[title];
+          return (
+            <CategoryPreview key={title} title={title} products={products} />
+          );
+        })
+      }
+    </Fragment>
+  );
+};
+
+export default CategoriesPreview;
---
+++ src/routes/category/category.component.jsx
@@ -0,0 +1,35 @@
+import { useParams } from "react-router-dom";
+import { useContext, useState, useEffect, Fragment } from "react";
+
+import ProductCard from "../../components/product-card/product-card.component";
+import { CategoriesContext } from "../../contexts/categories.context";
+import "./category.styles.scss";
+
+const Category = () => {
+  // to get category, i.e, jackets, hats etc using useParams()
+  const { category } = useParams();
+  const { categoriesMap } = useContext(CategoriesContext);
+
+  // We could also do products = categoriesMap[category];
+  // but it will render everytime, so we need it to re-render only when either
+  // the category or categoriesMap changes
+  const [products, setProducts] = useState(categoriesMap[category]);
+
+  useEffect(() => {
+    setProducts(categoriesMap[category]);
+  }, [category, categoriesMap]);
+
+  return (
+    <Fragment>
+      <h2 className="category-title">{category.toLocaleUpperCase()}</h2>
+      <div className="category-container">
+        {products &&
+          products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </Fragment>
+  );
+};
+
+export default Category;
